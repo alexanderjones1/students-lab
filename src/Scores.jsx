1173b1aa696db4cfa38712e3c91cb5155528d4e8
alex-jones <abjhoops@gmail.com>
2023-01-24 21:27:48 -0700

    import Scores to Student component

---
+++ src/Scores.jsx
@@ -0,0 +1,11 @@
+const Scores = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h4>This is a Scores component</h4>
+      
+    </>
+  )
+}
+
+export default Scores
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,9 +1,12 @@
+import Scores from "./Scores";
+
 const Student = (props) => {
   console.log(props.student.name);
   return (
     <>
       <h1>{props.student.name}</h1>
       <h3>{props.student.bio}</h3>
+      <Scores/>
       
     </>
   )
